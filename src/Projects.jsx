b187432ec73b563c450ffda944b8664fa2e8abdf
Clay O'Dell <clayodell.swe@gmail.com>
2025-05-21 23:34:49 -0500

    updated projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 const Projects = () => {
   return (
     <>
@@ -6,75 +5,72 @@ const Projects = () => {
       <ul>
         <li>
           <strong>Gig Match Calendar App</strong>
-        </li>
-        <p>
-          This is my capstone project from Auburn's Software Engineering
-          Bootcamp
-        </p>
-        <ul>
-          <li>
-            <a href="https://gigmatch-frontend-calendar-app.onrender.com/">
-              Application
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/clay-odell/GigMatch_Frontend_Calendar_App">
-              Frontend Code Base
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/clay-odell/GigMatch_Backend">Backend</a>
-          </li>
-        </ul>
-        <li>
-          <br />
-          <strong>Dutch Bros Shift Checklist App</strong>
-        </li>
-        <p>
-          This app is a "clicker" that assist a shift lead in their given shift
-          duties. It solves the problem of not having to go to a posted
-          checklist to accomplish shift tasks.
-        </p>
-        <ul>
-          <li>
-            <a href="https://clay-odell.github.io/dutchbrosChecklist/">
-              Application
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/clay-odell/dutchbrosChecklist">
-              Code Base
-            </a>
-          </li>
-          <li>
-            <br />
-            <strong>Dr. Whateva Website App</strong>
-          </li>
           <p>
-            This is a website that I built for my band Dr. Whateva, with both a
-            front- and backend. Users can sign up for a mailing list or request
-            a song for the band to cover.
+            This is my capstone project from Auburn's Software Engineering Bootcamp.
+          </p>
+          <ul>
+            <li>
+              <a href="https://gigmatch-frontend-calendar-app.onrender.com/">
+                Application
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/clay-odell/GigMatch_Frontend_Calendar_App">
+                Frontend Code Base
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/clay-odell/GigMatch_Backend">
+                Backend
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <strong>Dutch Bros Shift Checklist App</strong>
+          <p>
+            This app is a "clicker" that assists a shift lead with their shift duties. It solves the problem of having to refer to a posted checklist to accomplish shift tasks.
+          </p>
+          <ul>
+            <li>
+              <a href="https://clay-odell.github.io/dutchbrosChecklist/">
+                Application
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/clay-odell/dutchbrosChecklist">
+                Code Base
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <strong>Dr. Whateva Website App</strong>
+          <p>
+            This is a website that I built for my band Dr. Whateva, with both a front- and backend.
+            Users can sign up for a mailing list or request a song for the band to cover.
           </p>
           <ul>
             <li>
               <a href="https://drwhateva.com">
-              Website
+                Website
               </a>
             </li>
             <li>
               <a href="https://github.com/clay-odell/dr.whateva.com">
-              Frontend Code Base 
+                Frontend Code Base
               </a>
             </li>
             <li>
               <a href="https://github.com/clay-odell/whateva_backend">
-              Backend Code Base
+                Backend Code Base
               </a>
             </li>
           </ul>
-        </ul>
+        </li>
       </ul>
     </>
   );
 };
+
 export default Projects;
